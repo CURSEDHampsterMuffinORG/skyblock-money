bf0408aa95c15e1208b942f869060e816456f1da
Kendell R <KTibow@users.noreply.github.com>
2021-08-04 18:46:18 -0700

    discord and random api.py stuff

--- api.py
+++ api.py
@@ -5,13 +5,14 @@ API_KEY = os.environ["API_KEY"]
 
 class User:
   def __init__(self, username):
-    uuid = requests.get(
-      f"https://api.mojang.com/users/profiles/minecraft/{username}"
-    )
+    uuid = requests.get(f"https://api.mojang.com/users/profiles/minecraft/{username}").json()["id"]
     profiles = requests.get(
       "https://api.hypixel.net/skyblock/profiles",
       params={"uuid": uuid},
       headers={"Api-Key": API_KEY},
     )
-    self.uuid = uuid.json()["id"]
+    self.uuid = uuid
     self.profiles = profiles.json()["profiles"][0]
+
+  def get_flips(self):
+    ...
