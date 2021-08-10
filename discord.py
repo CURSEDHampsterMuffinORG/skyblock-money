72b7e12deb92e32b84864666af73fb3187e75cac
Kendell R <KTibow@users.noreply.github.com>
2021-08-10 15:03:08 +0000

    Repl fixes

--- discord.py
+++ discord.py
@@ -4,7 +4,10 @@ from requests_oauthlib import OAuth2Session
 
 OAUTH2_CLIENT_ID = os.environ["CLIENT_ID"]
 OAUTH2_CLIENT_SECRET = os.environ["CLIENT_SECRET"]
-OAUTH2_REDIRECT_URI = "http://localhost:5000/callback"
+if "REPL_ID" in os.environ:
+  OAUTH2_REDIRECT_URI = "https://skyblock-money.ktibow.repl.co/callback"
+else:
+  OAUTH2_REDIRECT_URI = "http://localhost:5000/callback"
 
 
 def token_updater(token):
@@ -44,10 +47,11 @@ def handle_callback():
       token = discord.fetch_token(
         "https://discordapp.com/api/oauth2/token",
         client_secret=OAUTH2_CLIENT_SECRET,
-        authorization_response=request.url,
+        authorization_response=request.url.replace("http:", "https:"),
       )
   except Exception as e:
-      return "<p style='font-family: sans-serif'>Error authing. Try clearing your cookies.</p>"
+    print(e)
+    return "<p style='font-family: sans-serif'>Error authing. Try clearing your cookies.</p>"
   resp = make_response(
     "<p style='font-family: sans-serif'>Token has been saved. <a href='/'>Get flips</a></p>"
   )
